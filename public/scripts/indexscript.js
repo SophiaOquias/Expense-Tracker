80e0a1c7da60003964bc50a2f487f500f126f6da
Sophia <sophia.oquias@gmail.com>
2022-06-07 09:40:16 +0800

    accomplished get expenses and getting total expenses

--- public/scripts/indexscript.js
+++ public/scripts/indexscript.js
@@ -1,3 +1,71 @@
+$(document).ready(function() {
+
+    function numberWithCommas(number) {
+        // regex formula from
+        // https://www.delftstack.com/howto/javascript/javascript-add-commas-to-number/
+        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+    
+    function createExpenseDiv(expense, parentDiv) {
+        // Guide Format 
+        // <div id="entry1" class="entry" onclick="location.href='viewentry.html';">
+        //     <span class="description">Thomas The Train Trainset</span>
+        //     <span class="entryType">Expense</span>
+        //     <br>
+        //     <span class="amount">P1,760.00</span>
+        //     <span class="date">January 03, 2022</span>
+        // </div>
+
+        // create html elements
+        var expenseEntry = document.createElement("div");
+        var descriptionRow = document.createElement("span");
+        var entryTypeRow = document.createElement("span");
+        var amountRow = document.createElement("span");
+        var dateRow = document.createElement("span");
+        var br = document.createElement("br");
+
+        // add css styles 
+        $(expenseEntry).addClass("entry");
+        $(descriptionRow).addClass("description");
+        $(entryTypeRow).addClass("entryType");
+        $(amountRow).addClass("amount");
+        $(dateRow).addClass("date");
+
+        // add data 
+        $(descriptionRow).text(expense.description);
+        $(entryTypeRow).text(expense.entryType);
+        $(amountRow).text(expense.amount);
+        $(dateRow).text(expense.date);
+
+        // append to parent div 
+        expenseEntry.append(descriptionRow);
+        expenseEntry.append(entryTypeRow);
+        expenseEntry.append(br);
+        expenseEntry.append(amountRow);
+        expenseEntry.append(dateRow);
+
+        parentDiv.append(expenseEntry);
+    }
+
+    $.get('get-expenses', function(data, status) {
+        console.log(data);
+
+        var expenseContainer = $("#bottomsection");
+
+        var sum = 0;
+
+        data.forEach((item, i) => {
+            createExpenseDiv(item, expenseContainer);
+
+            sum += item.amount;
+        });
+
+        // to .toFixed(2) adds decimal 
+        $("#totalExpenses").text("P" + numberWithCommas(sum.toFixed(2)));
+    });
+})
+
+// TO DO: make this jquery later
 function editBalance() {
     var editButton = document.getElementById("editbalance");
     var amount = document.getElementById("balamount");
