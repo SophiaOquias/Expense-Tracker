bc9558f31413f2ed9a29cf9b5390f30ac235a8e8
Sophia <sophia.oquias@gmail.com>
2022-06-08 09:51:17 +0800

    added views and routes to login, view account, view expense, view savings pages

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = new express();
 // init server port
 const port = 3000; 
 var server = app.listen(port, function () {
-    console.log("Listening at port " + port);
+    console.log("Listening at port " + port + "...");
 });
 
 // MONGOOSE stuff 
@@ -21,23 +21,23 @@ const path = require('path');
 // Initialize data and static folder 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })) // might change later
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public")); // place all html, js, css for the pages here
 
-/* using handlebars */
+// using handlebars 
 app.set('view engine', 'hbs');
 app.engine("hbs", exphbs.engine({ extname: "hbs" }));
 
 
 // ROUTES
 
-// INDEX STUFF
-// To do: edit goals and viewing entries 
+// INDEX STUFF (home page)
+// TO DO: edit goals and viewing entries 
 app.get('/', function(req, res) {
     // res.sendFile(__dirname + '\\' + 'public/html/index.html');
     res.render("index");
 });
 
-// for some reason this needs to be async, not sure why tho but it works ¯\_(ツ)_/¯
+// for some reason this needs to be async, not entirely sure why tho but it works ¯\_(ツ)_/¯
 app.get('/get-expenses', async(req, res) => {
     var expenses = await Post.find({}).lean(); // .lean() makes query in JSON format
     res.status(200).send(expenses);
@@ -52,6 +52,7 @@ app.post('/add-expense', function(req, res) {
     var expense = {
         // entryType is the name attr, and entrytype is id attr in hbs file
         // for some reason if element is a selection, it needs name attribute instead of id
+        // if id is used to identify selection, the selection won't be detected in the req.body
         entryType: req.body.entryType,
         date: req.body.date,
         category: req.body.category,
@@ -61,9 +62,29 @@ app.post('/add-expense', function(req, res) {
         ORnumber: req.body.ORnumber
     }
 
+    // create document in db 
     Post.create(expense);
 
+    // refer to public/scripts/newentry.js for all the AJAX stuff
     res.status(200).send(expense);
 });
 
-// to add: app.post for confirming new entry
+// VIEW EXPENSES STUFF
+app.get('/view-expenses', function(req, res) {
+    res.render("view-expenses");
+});
+
+// VIEW SAVINGS STUFF
+app.get('/view-savings', function(req, res) {
+    res.render("view-savings");
+});
+
+// LOGIN STUFF
+app.get('/login', function(req, res) {
+    res.render("login", {layout: "login-layout"});
+});
+
+// VIEW ACCOUNT STUFF
+app.get('/account', function(req, res) {
+    res.render("view-account");
+});
