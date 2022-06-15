ccfe5aa3b2f85eabb661e4defc125b58677f8769
rijopalla <rij_opalla@dlsu.edu.ph>
2022-06-15 22:27:53 +0800

    added loginUser and logoutUser

--- userController.js
+++ userController.js
@@ -27,3 +27,23 @@ exports.registerUser = (req, res) => {
     res.redirect('/signup');
   }
 };
+
+exports.loginUser = (req, res) => {
+  // 1. Validate request
+
+  // 2. If VALID, find if email exists in users
+  //      EXISTING USER (match retrieved)
+  //        a. Check if password matches hashed password in database
+  //        b. If MATCH, save info to session and redirect to home
+  //        c. If NOT equal, redirect to login page with error
+  //      UNREGISTERED USER (no results retrieved)
+  //        a. Redirect to login page with error message
+
+  // 3. If INVALID, redirect to login page with errors
+  res.redirect('/');
+};
+
+exports.logoutUser = (req, res) => {
+  // Destroy the session and redirect to login page
+  res.redirect('/login');
+};
