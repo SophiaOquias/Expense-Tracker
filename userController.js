bccd2a14a9cb9cc7498f93db3f1bfd02255d223e
rijopalla <rij_opalla@dlsu.edu.ph>
2022-06-21 19:06:03 +0800

    creates new user and hashed details

--- userController.js
+++ userController.js
@@ -1,31 +1,52 @@
 const { check } = require("express-validator");
 const userModel = require("./database/models/User");
 const {validationResult} = require('express-validator');
+const bcrypt = require('bcrypt');
 
 exports.registerUser = (req, res) => {
-  // 1. Validate request
-
-  // 2. If VALID, find if email exists in users
-  //      NEW USER (no results retrieved)
-  //        a. Hash password
-  //        b. Create user
-  //        c. Redirect to login page
-  //      EXISTING USER (match retrieved)
-  //        a. Redirect user to login page with error message.
-
-  // 3. If INVALID, redirect to register page with errors
   const errors = validationResult(req);
 
   if (errors.isEmpty()) {
-    const { email, username, password1 } = req.body;
-  
-    //TODO: Test validation
-    res.redirect('/login');
+    const {email, username, password} = req.body;
+
+    userModel.getOne({email: email}, (err, result) => {
+      if (result) {
+        console.log(result);
+
+        req.flash('error_msg', 'User already exists. Please login');
+        res.redirect('/login');
+      } else {
+        const saltRounds = 10;
+        
+        //Hash password
+        bcrypt.hash(password, saltRounds, (err, hashed) => {
+          const newUser = {
+            email,
+            username,
+            password: hashed
+          };
+
+          userModel.create(newUser, (err, user) => {
+            if (err) {
+              req.flash('error_msg', 'Could not create user. Please try again.');
+              res.redirect('signup');
+            } else {
+              req.flash('success_msg', 'You are now registered!');
+              res.redirect('/login');
+            }
+          })
+        })
+
+      }
+    })
   } else {
-  
-    req.flash('error_msg', "Error");
+    //error
+    const messages = errors.array().map((item) => item.msg);
+    req.flash('error_msg', messages.join(' '));
+    console.log(messages.join(' '));
     res.redirect('/signup');
-  };
+  }
+
 }
 
 exports.loginUser = (req, res) => {
@@ -35,7 +56,7 @@ exports.loginUser = (req, res) => {
     const {username, password} = req.body;
 
     // Next items go here... Same as before, this will be replaced.
-    userModel.getOne({ username: usernam }, (err, User) => {
+    userModel.getOne({ username: username }, (err, User) => {
       if (err) {
         // Database error occurred...
         req.flash('error_msg', 'Something happened! Please try again.');
